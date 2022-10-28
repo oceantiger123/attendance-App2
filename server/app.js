93150f911f10e31523340c3f050a36c3acbeff38
Ben Ye <oceanblueye@gmail.com>
2022-10-28 17:12:48 -0400

    changed app.js

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 // sends index.html
 app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 })
 
 // error handling endware
